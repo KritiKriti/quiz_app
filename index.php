bb3f1dea20eeed13a6cb8ee5e97389be589596fc
KritiKriti <basnetkriti83@gmail.com>
2026-05-30 11:51:07 +0545

    basics

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz App</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="home">
+
+    <h1>Online Quiz Application</h1>
+
+    <a href="login.php"><button>Login</button></a>
+    <a href="register.php"><button>Register</button></a>
+
+</div>
+
+</body>
+</html>
