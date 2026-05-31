c9851e566a125c9c60222dfb69e9ff91ca410b66
KritiKriti <basnetkriti83@gmail.com>
2026-05-31 19:47:37 +0545

    ..

--- quiz.php
+++ quiz.php
@@ -1,40 +1,55 @@
 <?php
-include("../config/db.php");
+include("db.php");
 
-$subject = $_GET['sid'];
+$subject = $_GET['subject'] ?? '';
 
-$res = mysqli_query($conn,
-"SELECT * FROM questions WHERE subject_id=$subject ORDER BY RAND()");
+$sql = "SELECT * FROM questions 
+        WHERE subject_id = (
+            SELECT id FROM subjects WHERE subject_name='$subject'
+        )
+        ORDER BY RAND()
+        LIMIT 10";
 
+$result = mysqli_query($conn, $sql);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="../css/style.css">
+    <title>Quiz</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<div class="card">
+<div class="quiz-container">
 
-<h2>Quiz</h2>
+    <h2><?php echo $subject; ?> Quiz</h2>
 
-<form action="result.php" method="POST">
+    <form action="result.php" method="POST">
 
-<?php while($q = mysqli_fetch_assoc($res)) { ?>
+        <?php $qno = 1; while($row = mysqli_fetch_assoc($result)) { ?>
 
-    <p><?php echo $q['question']; ?></p>
+            <div class="question-box">
 
-    <div class="option"><?php echo $q['option1']; ?></div>
-    <div class="option"><?php echo $q['option2']; ?></div>
-    <div class="option"><?php echo $q['option3']; ?></div>
-    <div class="option"><?php echo $q['option4']; ?></div>
+                <p>
+                    <?php echo $qno . ". " . $row['question']; ?>
+                </p>
 
-<?php } ?>
+                <label><input type="radio" name="q<?php echo $row['id']; ?>" value="A"> <?php echo $row['option_a']; ?></label><br>
 
-<button type="submit">Submit Quiz</button>
+                <label><input type="radio" name="q<?php echo $row['id']; ?>" value="B"> <?php echo $row['option_b']; ?></label><br>
 
-</form>
+                <label><input type="radio" name="q<?php echo $row['id']; ?>" value="C"> <?php echo $row['option_c']; ?></label><br>
+
+                <label><input type="radio" name="q<?php echo $row['id']; ?>" value="D"> <?php echo $row['option_d']; ?></label>
+
+            </div>
+
+        <?php $qno++; } ?>
+
+        <button type="submit">Submit Quiz</button>
+
+    </form>
 
 </div>
 
